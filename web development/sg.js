7e69da1e1f56796c0f6a53afbeb104a508f32551
shivani5634 <shivanigupta5634769@mail.com>
2021-05-11 15:27:35 +0530

    Initial commit

---
+++ web development/sg.js
@@ -0,0 +1,29 @@
+/*let elem=document.getElementsByClassName("h-primary");
+console.log(elem);
+elem[0].classList.add("bg-primary");
+console.log(elem[0].innerHTML);
+console.log(elem[0].innerText);
+    
+
+*/
+/*tn=document.getElementsByTagName("div")
+console.log(tn);
+createdElement = document.createElement("p");
+createdElement.innerText="this is a created para";
+tn[0].appendChild(createdElement);
+createdElement1 = document.createElement("b");
+createdElement1.innerText="this is a created bold";
+tn[0].replaceChild(createdElement1,createdElement);
+
+*/
+// window.location="next.html";
+function Redirect() {
+    //window.location = "https://www.tutorialspoint.com";
+    window.location = "next.html";
+
+}
+function display() {
+    //window.location = "https://www.tutorialspoint.com";
+    window.location = "new.html";
+
+}
